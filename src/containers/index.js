d92103f8daea2ee56fe01dd77b1a90f89c3752f8
Wouter de Vos <wouter@codaisseur.com>
2017-10-02 17:42:07 +0200

    Revert "Super Duper One!"
    This reverts commit 3bacd8458aee2c4961009f2bf84c907a5ac3fc1e.

--- src/containers/index.js
+++ src/containers/index.js
@@ -2,14 +2,10 @@ import Game from './Game'
 import Lobby from './Lobby'
 import SignIn from './SignIn'
 import SignUp from './SignUp'
-import SuperDuperTwo from './SignUp'
-import SuperDuperOne from './SignUp'
 
 export {
   Game,
   Lobby,
   SignIn,
   SignUp,
-  SuperDuperTwo,
-  SuperDuperOne,
 }
