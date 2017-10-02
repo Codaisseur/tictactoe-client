3bacd8458aee2c4961009f2bf84c907a5ac3fc1e
Wouter de Vos <wouter@codaisseur.com>
2017-10-02 17:34:06 +0200

    Super Duper One!

--- src/containers/index.js
+++ src/containers/index.js
@@ -2,10 +2,12 @@ import Game from './Game'
 import Lobby from './Lobby'
 import SignIn from './SignIn'
 import SignUp from './SignUp'
+import SuperDuperOne from './SignUp'
 
 export {
   Game,
   Lobby,
   SignIn,
   SignUp,
+  SuperDuperOne,
 }
